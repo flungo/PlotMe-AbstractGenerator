db7bdad8749f94aeb218e7bed194f39e1f99521c
Fabrizio Lungo <fab@lungo.co.uk>
2013-11-11 14:34:24 +0000

    Implemented getFixedSpawnLocation in AbstractChunkGenerator

--- src/main/java/me/flungo/bukkit/plotme/abstractgenerator/AbstractChunkGenerator.java
+++ src/main/java/me/flungo/bukkit/plotme/abstractgenerator/AbstractChunkGenerator.java
@@ -9,6 +9,9 @@
 package me.flungo.bukkit.plotme.abstractgenerator;
 
 import com.worldcretornica.plotme_core.api.v0_14b.IPlotMe_ChunkGenerator;
+import java.util.Random;
+import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.generator.ChunkGenerator;
 
 /**
@@ -16,5 +19,18 @@ import org.bukkit.generator.ChunkGenerator;
  * @author Fabrizio Lungo <fab@lungo.co.uk>
  */
 public abstract class AbstractChunkGenerator extends ChunkGenerator implements IPlotMe_ChunkGenerator {
+
+    private final WorldGenConfig wgc;
+    private final AbstractGenerator plugin;
+
+    public AbstractChunkGenerator(AbstractGenerator instance, WorldGenConfig wgc) {
+        plugin = instance;
+        this.wgc = wgc;
+    }
+
+    @Override
+    public Location getFixedSpawnLocation(World world, Random random) {
+        return new Location(world, wgc.getInt("XTranslation"), wgc.getInt("RoadHeight") + 2, wgc.getInt("ZTranslation"));
+    }
 
 }
