49fd995586295caedd842e2e40bbe7f641bbd432
Fabrizio Lungo <fab@lungo.co.uk>
2013-11-11 13:20:21 +0000

    Create BlockInfo with BlockRepresentation, World and x, y, z co-ordinates.

--- src/main/java/me/flungo/bukkit/plotme/abstractgenerator/BlockInfo.java
+++ src/main/java/me/flungo/bukkit/plotme/abstractgenerator/BlockInfo.java
@@ -1,6 +1,7 @@
 package me.flungo.bukkit.plotme.abstractgenerator;
 
 import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.block.Block;
 
 public class BlockInfo {
@@ -11,6 +12,10 @@ public class BlockInfo {
     public BlockInfo(BlockRepresentation block, Location loc) {
         this.block = block;
         this.loc = loc;
+    }
+
+    public BlockInfo(BlockRepresentation block, World w, int x, int y, int z) {
+        this(block, new Location(w, x, y, z));
     }
 
     public BlockInfo(Block block) {
