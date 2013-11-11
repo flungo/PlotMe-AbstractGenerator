3633d96d0c1d4bd2188ae2eebd60ee1499cfb28e
Fabrizio Lungo <fab@lungo.co.uk>
2013-11-11 14:09:57 +0000

    Created empty AbstractChunkGenerator

---
+++ src/main/java/me/flungo/bukkit/plotme/abstractgenerator/AbstractChunkGenerator.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) 2013 Fabrizio Lungo <fab@lungo.co.uk> - All Rights Reserved
+ *
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ *
+ * Created by Fabrizio Lungo <fab@lungo.co.uk>, November 2013
+ */
+package me.flungo.bukkit.plotme.abstractgenerator;
+
+import com.worldcretornica.plotme_core.api.v0_14b.IPlotMe_ChunkGenerator;
+import org.bukkit.generator.ChunkGenerator;
+
+/**
+ *
+ * @author Fabrizio Lungo <fab@lungo.co.uk>
+ */
+public abstract class AbstractChunkGenerator extends ChunkGenerator implements IPlotMe_ChunkGenerator {
+
+}
