7fdbe62683aa514466cff765ad0ffedf45a9744b
Fabrizio Lungo <fab@lungo.co.uk>
2013-11-12 00:52:14 +0000

    Added accessors for Core Config and Captions

--- src/main/java/me/flungo/bukkit/plotme/abstractgenerator/AbstractGenerator.java
+++ src/main/java/me/flungo/bukkit/plotme/abstractgenerator/AbstractGenerator.java
@@ -10,9 +10,12 @@ package me.flungo.bukkit.plotme.abstractgenerator;
 
 import com.worldcretornica.plotme_core.api.v0_14b.IPlotMe_GeneratorManager;
 import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
+import java.util.logging.Level;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
@@ -22,18 +25,28 @@ import org.bukkit.plugin.java.JavaPlugin;
 public abstract class AbstractGenerator extends JavaPlugin {
 
     public static final String CORE_PLUGIN_NAME = "PlotMe";
+    public static final String CORE_CONFIG_NAME = "core-config.yml";
+    public static final String CORE_LANG_PATH = "Language";
+    public static final String CORE_DEFAULT_LANG = "english";
+    public static final String CORE_CAPTIONS_PATTERN = "caption-%s.yml";
     public static final String DEFAULT_CONFIG_NAME = "config.yml";
+    public static final String DEFAULT_LANG = "english";
     public static final String DEFAULT_CAPTIONS_FILE = "caption-english.yml";
     public static final String WORLDS_CONFIG_SECTION = "worlds";
 
+    private File coreFolder;
     private File configFolder;
 
+    private File coreConfigFile;
+
+    private FileConfiguration coreConfig;
+    private HashMap<String, FileConfiguration> coreCaptions;
     private ConfigAccessor configCA;
     protected ConfigAccessor captionsCA;
 
     @Override
     public final void onEnable() {
-        setupConfigFolder();
+        setupConfigFolders();
         setupConfig();
         initialize();
     }
@@ -50,11 +63,76 @@ public abstract class AbstractGenerator extends JavaPlugin {
 
     public abstract void takedown();
 
-    private void setupConfigFolder() {
+    private void setupConfigFolders() {
         File pluginsFolder = getDataFolder().getParentFile();
-        File plotMeFolder = new File(pluginsFolder, CORE_PLUGIN_NAME);
-        this.configFolder = new File(plotMeFolder, getName());
-        this.configFolder.mkdirs();
+        coreFolder = new File(pluginsFolder, CORE_PLUGIN_NAME);
+        coreConfigFile = new File(coreFolder, CORE_CONFIG_NAME);
+        configFolder = new File(coreFolder, getName());
+        configFolder.mkdirs();
+    }
+
+    public File getCoreFolder() {
+        return coreFolder;
+    }
+
+    public void reloadCoreConfig() {
+        coreConfig = YamlConfiguration.loadConfiguration(coreConfigFile);
+    }
+
+    public FileConfiguration getCoreConfig() {
+        if (coreConfig == null) {
+            reloadCoreConfig();
+        }
+        return coreConfig;
+    }
+
+    public void saveCoreConfig() {
+        if (coreConfig == null || coreConfigFile == null) {
+            return;
+        }
+        try {
+            getConfig().save(coreConfigFile);
+        } catch (IOException ex) {
+            getLogger().log(Level.SEVERE, "Could not save config to " + coreConfigFile, ex);
+        }
+    }
+
+    private File getCoreCaptionsFile(String lang) {
+        return new File(coreFolder, String.format(CORE_CAPTIONS_PATTERN, lang));
+    }
+
+    public void reloadCoreCaptions() {
+        reloadCoreCaptions(getCoreConfig().getString(CORE_LANG_PATH, CORE_DEFAULT_LANG));
+    }
+
+    public void reloadCoreCaptions(String lang) {
+        coreCaptions.put(lang, YamlConfiguration.loadConfiguration(getCoreCaptionsFile(lang)));
+    }
+
+    public FileConfiguration getCoreCaptions() {
+        return getCoreCaptions(getCoreConfig().getString(CORE_LANG_PATH, CORE_DEFAULT_LANG));
+    }
+
+    public FileConfiguration getCoreCaptions(String lang) {
+        if (!coreCaptions.containsKey(lang)) {
+            reloadCoreConfig();
+        }
+        return coreCaptions.get(lang);
+    }
+
+    public void saveCoreCaptions() {
+        saveCoreCaptions(getCoreConfig().getString(CORE_LANG_PATH, CORE_DEFAULT_LANG));
+    }
+
+    public void saveCoreCaptions(String lang) {
+        if (!coreCaptions.containsKey(lang)) {
+            return;
+        }
+        try {
+            coreCaptions.get(lang).save(getCoreCaptionsFile(lang));
+        } catch (IOException ex) {
+            getLogger().log(Level.SEVERE, "Could not save config to " + coreConfigFile, ex);
+        }
     }
 
     public File getConfigFolder() {
@@ -63,7 +141,7 @@ public abstract class AbstractGenerator extends JavaPlugin {
 
     @Override
     public void reloadConfig() {
-        configCA.reloadConfig(); //To change body of generated methods, choose Tools | Templates.
+        configCA.reloadConfig();
     }
 
     @Override
